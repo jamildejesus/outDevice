4fd8664c2efd1727e9b0c6418124f101573dc006
jamildejesus <jamildejesusenriquezdeceano@gmail.com>
2023-05-14 23:27:39 -0600

    add backend dev

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiURL: 'http://192.168.1.146:8000' //IP DEL BACKEND
+    //apiURL: 'http://192.168.1.146:8000' //IP DEL BACKEND
+    apiURL: 'http://187.146.38.66:8000' //IP DEL BACKEND
   };
